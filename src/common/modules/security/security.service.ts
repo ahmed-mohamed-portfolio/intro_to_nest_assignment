f635513b5a59f48dad0c48eae7a73409984a7f87
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-07-10 17:51:16 +0300

    enh

--- src/common/modules/security/security.service.ts
+++ src/common/modules/security/security.service.ts
@@ -6,31 +6,18 @@ import crypto from 'node:crypto';
 @Injectable()
 export class securityService {
   constructor(private readonly configService: ConfigService) {}
-
-  generateHash = async ({
-    plaintext,
-    salt = Number(this.configService.get<string>('SALT_ROUND')),
-  }: {
-    plaintext: string;
-    salt?: number;
-  }): Promise<string> => {
+  //Hash converts data into a one-way value that cannot be reversed, so it is used for passwords or OTPs, and comparison is done using bcrypt.compare() without decrypting anything.
+  generateHash = async ({ plaintext, salt = Number(this.configService.get<string>('SALT_ROUND')) }: { plaintext: string; salt?: number }): Promise<string> => {
     return await hash(plaintext, salt);
   };
 
-  compareHash = async ({
-    plaintext,
-    cipherText,
-  }: {
-    plaintext: string;
-    cipherText: string;
-  }): Promise<boolean> => {
+  compareHash = async ({ plaintext, cipherText }: { plaintext: string; cipherText: string }): Promise<boolean> => {
     return await compare(plaintext, cipherText);
   };
 
+  //Encryption is reversible, meaning you can get the original data back using a secret key, so it is used for data that needs to be read again later.
   generateEncryption = async (plaintext: string): Promise<string> => {
-    const iv = crypto.randomBytes(
-      Number(this.configService.get<string>('ENC_IV_LENGTH')),
-    );
+    const iv = crypto.randomBytes(Number(this.configService.get<string>('ENC_IV_LENGTH')));
 
     const ENC_KEY = this.configService.get<string>('ENC_KEY') as string;
 
@@ -52,11 +39,7 @@ export class securityService {
     const ivLikeBinary = Buffer.from(iv, 'hex');
     console.log({ iv, ivLikeBinary, encryption });
 
-    const deCipherIvVector = crypto.createDecipheriv(
-      'aes-256-cbc',
-      ENC_KEY,
-      ivLikeBinary,
-    );
+    const deCipherIvVector = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, ivLikeBinary);
     let plaintext = deCipherIvVector.update(encryption, 'hex', 'utf-8');
     plaintext += deCipherIvVector.final('utf-8');
     return plaintext;
